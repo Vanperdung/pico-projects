cc50f814f5bc17bb600de8908e488406ce1501f7
Vanperdung <dung.nv382001@gmail.com>
2024-04-24 00:21:15 +0700

    add support uart

---
+++ watch/hal/hal_uart.h
@@ -0,0 +1,16 @@
+#ifndef _UART_H_
+#define _UART_H_
+
+#include <stdio.h>
+#include "hardware/uart.h"
+#include "pico/stdlib.h"
+
+#define UART_ID uart0
+#define BAUD_RATE 115200
+
+#define UART_TX_PIN 16
+#define UART_RX_PIN 17
+
+void hal_uart_init(void);
+
+#endif
--- watch/main.c
+++ watch/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/uart.h"
 #include "lcd/lcd.h"
 #include "lvgl/lvgl.h"
 #include "hal/hal_lcd.h"
-#include "lvgl/examples/lv_examples.h"
+#include "hal/hal_uart.h"
 
 int main(void)
 {
+    stdio_init_all();
+    hal_uart_init();
     lv_init();
-
+    printf("-------------------Setup done-------------------\r\n");
+    
     while (1)
     {
         lv_timer_handler();
