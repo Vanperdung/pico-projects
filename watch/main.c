f9c6a74605378a9b3380d879d8fe94516edfea78
Vanperdung <dung.nv382001@gmail.com>
2024-04-23 08:06:20 +0700

    update CMakeLists for lvgl, built successfully

--- watch/main.c
+++ watch/main.c
@@ -2,13 +2,15 @@
 #include "pico/stdlib.h"
 #include "lcd/lcd.h"
 #include "lvgl/lvgl.h"
+#include "lvgl/examples/lv_examples.h"
 
 int main(void)
 {
-
-    
+    lv_init();
     while (1)
     {
-
+        lv_timer_handler();
+        sleep_ms(5);
     }
+    return 0;
 }
