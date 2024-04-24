c4cd286cfb368ecfe87121eabfa70d140d127b48
Vanperdung <dung.nv382001@gmail.com>
2024-04-25 00:10:07 +0700

    implement timer

---
+++ watch/timer/sw_timer.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "sw_timer.h"
+
+bool repeating_timer_init(int32_t delay_ms, repeating_timer_callback_t timer_cb, void *user_data, repeating_timer_t *timer)
+{  
+    return add_repeating_timer_ms(delay_ms, timer_cb, user_data, timer);
+}
