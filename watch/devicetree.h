bdde84924d5c0d25810e3d7b398b952a5a18f120
Vanperdung <dung.nv382001@gmail.com>
2024-04-29 17:12:09 +0700

    support functions for gpio

---
+++ watch/devicetree.h
@@ -0,0 +1,20 @@
+#ifndef _DEVICE_TREE_H_
+#define _DEVICE_TREE_H_
+
+#define LCD_UART_ID uart0
+#define LCD_UART_BAUDRATE 912000
+
+#define LCD_UART_TX 16
+#define LCD_UART_RX 17
+
+#define LCD_DC 8
+#define LCD_SPI_CS 9
+#define LCD_SPI_CLK 10
+#define LCD_SPI_MOSI 11
+#define LCD_SPI_MISO 12
+#define LCD_RST 13
+#define LCD_BL 25
+
+#define SPI_ID spi1
+
+#endif
--- watch/spi/sw_spi.h
+++ watch/spi/sw_spi.h
@@ -1,16 +1,11 @@
 #ifndef _SW_SPI_H_
 #define _SW_SPI_H_
 
-#define LCD_DC_PIN 8
-#define LCD_CS_PIN 9
-#define LCD_CLK_PIN 10
-#define LCD_MOSI_PIN 11
-#define LCD_MISO_PIN 12
-#define LCD_RST_PIN 13
-#define LCD_BL_PIN 25
+#include "../devicetree.h"
 
-#define LCD_SPI_PORT spi1
+typedef unsigned int uint;
 
-void sw_spi_init(
+void sw_spi_init(uint baudrate);
+
 
 #endif
